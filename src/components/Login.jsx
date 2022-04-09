320561e3b4479aa202b0f08ecf34dd6f20544036
panfilo macantan <olifnapnatnacam27.com>
2022-04-09 03:02:18 -0300

    added login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
-
 const Login = () => {
-    return(
-        <h1>LOGIN PAGE</h1>
-    )
-}
+  return (
+    <div className="form_wrapper">
+      <form className="login_form">
+        <h3>Login</h3>
+        <div className="form_group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" className="form_control" />
+        </div>
+        <div className="form_group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" className="form_control" />
+        </div>
+        <div className="form_group">
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
